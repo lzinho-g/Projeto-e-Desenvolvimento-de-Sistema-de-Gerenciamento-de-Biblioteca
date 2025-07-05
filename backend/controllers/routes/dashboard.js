e69143ef4ed25bdf05461dd0dc21283939197a95
lzinho-g <luizhenrigarcia.contato@gmail.com>
2025-07-05 16:22:14 -0300

    Delete backend/controllers directory

--- backend/controllers/routes/dashboard.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getDashboardData, getLivrosRecentes } from '../controllers/dashboardController.js';
-
-const router = express.Router();
-
-router.get('/dados', getDashboardData);
-router.get('/livros-recentes', getLivrosRecentes);
-
-export default router;
